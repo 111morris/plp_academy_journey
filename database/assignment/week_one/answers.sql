987508418774564a7778c782b0cd5324aac1c52e
mulandi <mulandimorris1@gmail.com>
2025-08-14 00:11:39 +0300

    updated week one assignment

---
+++ database/assignment/week_one/answers.sql
@@ -0,0 +1,7 @@
+-- Steps to follow create a database called salesDB and drop it 
+
+-- this commands creates a database
+CREATE DATABASE salesDB;
+
+-- this command drops/deletes a daabase called demo
+DROP DATABASE demo;
