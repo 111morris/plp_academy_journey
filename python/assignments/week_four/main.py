c46b8ff6cf3f376de1e2433093eeae86866c57d9
mulandi <mulandimorris1@gmail.com>
2025-08-23 21:05:58 +0300

    done week four assignment python

--- python/assignments/week_four/main.py
+++ python/assignments/week_four/main.py
@@ -1,32 +1,48 @@
-def read_and_modify_file():
-    # this will ask the user to enter the filename
-    filename = input('Enter the name of the file to read: ')
-
-    try: 
-        # this will try to open the file 
-        infile = open(filename, 'r')
-        content = infile.read()
-        infile.close()
-
-        # this will modify content of the file 
-        modified_content = content.upper()
-
-        # this will open the output file manually 
-        outfile = open('modified_output.txt', 'w')
-        outfile.write(modified_content)
-        outfile.close()
-
-        print("File read successfully and modified content written to 'modified_output.txt'.")
-
-
-        # this will modify the content to make it uppercase
-        modified_content = content.upper()
-    except FileNotFoundError: 
-        print("There is an error, The file not found. Please check the filename and try again")
-    except PermissionError: 
-        print("There is an error, You don't have the permission to read this file")
-    except Exception as e:
-        print(f"An error occured: {e}")
+def file_manager():
+    while True:
+        # This will ask the user which action to take
+        print('''\nWelcome to Morris' file manager (CLI based) 
     
+What action do you want to do?
+1. Create a new file
+2. Open an existing file 
+3. Exit
+''')
 
-read_and_modify_file()
+        choice = input("Enter 1, 2 or 3: ")
+
+        if choice == "1": 
+            filename = input("Enter the name of the new file (e.g. myfile.txt): ")
+            try:
+                content = input("Enter the content to write in the file:\n")
+                file = open(filename, 'w')
+                file.write(content)
+                file.close()
+                print(f"File '{filename}' created successfully!")
+            except Exception as e: 
+                print(f"Error creating the file: {e}")
+        
+        elif choice == "2": 
+            filename = input("Enter the name of the file to open: ")
+            try: 
+                file = open(filename, 'r')
+                content = file.read()
+                file.close()
+                print(f"""\nFile Content: 
+====================
+{content}
+====================""")
+            except FileNotFoundError:
+                print("File not found. Please make sure the filename is correct.")
+            except Exception as e: 
+                print(f"Error reading the file: {e}")
+
+        elif choice == "3":
+            print("Exiting the file manager. Goodbye!")
+            break  # Exit the loop
+
+        else: 
+            print("Invalid choice. Please enter 1, 2 or 3.")
+
+file_manager()
+
